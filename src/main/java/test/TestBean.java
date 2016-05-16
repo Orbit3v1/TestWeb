4816ff214cd3572ddda3402602360d10ccd3a0d1
ayaroslavtsev <duke007@ukr.net>
2016-05-16 17:49:26 +0300

    find method to go through javax.persistence.RollbackException: Transaction marked as rollbackOnly error

--- src/main/java/test/TestBean.java
+++ src/main/java/test/TestBean.java
@@ -2,16 +2,19 @@ package test;
 
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Named;
 import javax.persistence.EntityManager;
+import javax.persistence.OptimisticLockException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
 @Named("testBean")
 @Scope("request")
+
 public class TestBean {
 
     private String name = "test";
@@ -46,4 +49,25 @@ public class TestBean {
         em.merge(new TestEntity("test"));
         int a = 1;
     }
+
+    public void errorCheck(){
+        errorMethod();
+    }
+
+
+    public void errorMethod(){
+        try{
+            methodC();
+        } catch (Exception e){
+            System.out.println("sdf");
+        }
+    }
+
+
+    @Transactional
+    public void methodC(){
+        em.persist(new TestEntity());
+    }
+
+
 }
